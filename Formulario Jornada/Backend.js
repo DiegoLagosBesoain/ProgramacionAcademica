b2335b5032051138898838829340fa4cacb804df
DiegoLagosBesoain <dnlagos@miuandes.cl>
2025-01-14 19:03:02 -0300

    Arreglado scripts hono

--- Formulario Jornada/Backend.js
+++ Formulario Jornada/Backend.js
@@ -1,4 +1,4 @@
-var id_hoja_maestro='1o6HftjnQiU4EB1T9mwZ5FntfkZqy9Bj5wkZKbyHl-m0'
+var id_hoja_maestro="1o6HftjnQiU4EB1T9mwZ5FntfkZqy9Bj5wkZKbyHl-m0"
 function doGet() {
   return HtmlService.createTemplateFromFile('index').evaluate();
 }
@@ -18,7 +18,7 @@ function pedirRut(nombre) {
     return {
 
         success: true, // Indica que la operación fue exitosa.
-        message: `Hola, ${nombre}. Ahora ingresa tu RUT (sin puntos ni guión).`
+        message: `Hola, ${nombre}. Ahora ingrese su RUT (sin puntos ni guión).`
        };
   } catch (e) {
     // Devuelve un mensaje de error en caso de que ocurra algún problema.
@@ -35,8 +35,29 @@ function validarRut(rut,nombre) {
   const data_profesores = obtener_data_profesor(rut);
   try{
   if (data_profesores.length < 1) {
-    throw new Error("El rut ingresado no es valido(si es guion 'k' ingreselo en mayusculas)."); // Lanza un error si el rut no es válido.
+    throw new Error("El rut ingresado no es valido(si es guión 'K' ingreselo en mayúsculas)."); // Lanza un error si el rut no es válido.
     }
+  const ss = SpreadsheetApp.openById(id_hoja_maestro)
+  const hoja = ss.getSheetByName("ENTREGADOS");
+  
+  if (!hoja) {
+    Logger.log("La hoja 'ENTREGADOS' no existe.");
+    return { success: false, message: "La hoja 'ENTREGADOS' no existe." };
+  }
+
+  // Obtener los valores de la primera columna (RUTs registrados)
+  const lastRow = hoja.getLastRow();
+  let rutColumna = [];
+  
+  if (lastRow > 0) {
+    // Obtener los valores de la primera columna solo si hay datos
+    rutColumna = hoja.getRange(1, 1, lastRow).getValues().flat();
+  }
+  console.log(rut,rutColumna)
+  // Verificar si el RUT ya está registrado
+  if (rutColumna.some((dato)=>dato==rut)) {
+    return { success: false, message: "Este RUT ya fue registrado previamente." };
+  }
   const semestres_protegidos_minor=["2","3","4","5"]
   const horas_totales = data_profesores.reduce((total, actual) => 
   actual[4] == 0 
@@ -105,7 +126,7 @@ function getPreguntaDinamica(nombre, data_profesor,horas_totales) {
     ? `${curso_seccion[6]} horas de taller/laboratorio` 
     : `${curso_seccion[4]} horas de clases`
 }`);
-  return 'Hola ' + nombre + ', este semestre has sido designado para realizar los cursos:\n -' + cursos_por_realizar.join("\n-")+"\nCon un total de "+ horas_totales + " Horas a realizar.\nPor favor indique su disponibilidad, mientras mas horas indique facilita el proceso de asignacion de horarios";
+   return 'Hola ' + nombre + ', este semestre has sido designado para realizar los cursos:\n -' + cursos_por_realizar.join("\n-")+"\nCon un total de "+ horas_totales + " Horas a realizar.\nPor favor indique su disponibilidad, mientras más horas indique facilita el proceso de asignación de horarios";
 }
 
 // Función para guardar las respuestas
@@ -161,7 +182,7 @@ function guardarHorariosEnHoja(nombre, rut, horarios) {
 }
 function guardarPreferenciasHoras(rut, preferencias) {
   try {
-    const hoja = SpreadsheetApp.openById().getSheetByName("PREFERENCIAS");
+    const hoja = SpreadsheetApp.openById(id_hoja_maestro).getSheetByName("PREFERENCIAS");
     preferencias.forEach(preferencia => {
       hoja.appendRow([rut, ...preferencia.curso.split(" "), preferencia.preferencia, new Date()]);
     });
